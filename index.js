b29ab2a6e488cd216ef94a27d3f4bd30d4438458
Emilie Ickart <emi.ickart@gmail.com>
2022-05-23 15:08:10 +0200

    Move index.js on main directory

---
+++ index.js
@@ -0,0 +1,20 @@
+/**
+ * Initialiser nodejs avec express et mongodb
+ * Doc : https://expressjs.com/fr/starter/hello-world.html
+ * @type {e | (() => Express)}
+ */
+
+const express = require('express');
+const server = express();
+const port = process.env.PORT || 3000;
+
+server.use(express.static('app'));
+server.use('/node_modules', express.static(__dirname + '/node_modules'));
+
+server.get('/', (req, res) => {
+    res.sendFile('index.html', { root: __dirname });
+});
+
+server.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
